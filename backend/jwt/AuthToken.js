410808270b4297e061c1113a368e7b48ee921740
zk3762100@gmail.com <132770259+zeeshandeveloper3762@users.noreply.github.com>
2024-09-04 06:50:14 +0500

    try to deployment

---
+++ backend/jwt/AuthToken.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+const createTokenAndSaveCookies = async (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "7d",
+  });
+  res.cookie("jwt", token, {
+   httpOnly: false, // Temporarily set to false for testing so that we can token in frontend
+    secure: false,
+    sameSite: "none",
+  });
+  await User.findByIdAndUpdate(userId, { token });
+  return token;
+};
+
+export default createTokenAndSaveCookies;
